a500a565f3c773790b12a47b4999e80fcc84b1b6
Krestine H <krha0001@stud.kea.dk>
2023-10-11 12:03:45 +0200

    fixed top spacing in main

--- src/scripts/java.js
+++ src/scripts/java.js
@@ -1,9 +1,11 @@
 const burgerMenu = document.querySelector(".burgermenu");
 const navMenu = document.querySelector(".burger_nav");
+const marginActive = document.querySelector("main");
 
 burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("active");
   navMenu.classList.toggle("active");
+  marginActive.classList.toggle("active");
 });
 
 document.querySelector(".nav_link").addEventListener("click", remove);
@@ -11,4 +13,5 @@ document.querySelector(".nav_link").addEventListener("click", remove);
 function remove() {
   burgerMenu.classList.remove("active");
   navMenu.classList.remove("active");
+  marginActive.classList.remove("active");
 }
